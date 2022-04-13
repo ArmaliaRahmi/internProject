77d6e6744828b3b028037460d0f16dd8ea144103
ArmaliaRahmi <armaliarahmi@gmail.com>
2022-04-13 19:53:11 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -42,4 +42,12 @@ $(".toggle-password").click(function() {
 });
 
 // captcha
-
+$('.reload').click(function () {
+  $.ajax({
+      type: 'GET',
+      url: 'reload-captcha',
+      success: function (data) {
+          $(".captcha span").html(data.captcha);
+      }
+  });
+});
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,12 +2,11 @@
 @section('ourcode')
 
 <div class="row">
-	<div class="col-md-5 mx-auto">
+	<div class="card-md-5 mx-auto">
 		<div id="second">
 			<div class="formRegis form">
 				<div class="logo mb-3">
 					<div class="col-md-12 text-center">
-                        
 						<h1>Registration Form</h1>
 					</div>
 				</div>
